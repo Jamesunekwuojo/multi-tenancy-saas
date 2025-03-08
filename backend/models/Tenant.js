b9e3fe527c7a51e67fc24ab5837ad22b56f8f71a
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-03-08 11:07:41 +0100

    reddis connection URl added

--- backend/models/Tenant.js
+++ backend/models/Tenant.js
@@ -5,6 +5,11 @@ const tenantSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  adminId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   subdomain: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
 });
